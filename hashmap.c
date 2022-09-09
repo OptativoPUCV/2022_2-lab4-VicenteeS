1a4754095ab3a0439cde9412fee746d30d32e32d
Replit user <>
2022-09-09 12:50:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -51,9 +51,12 @@ void enlarge(HashMap * map) {
 }
 
 
-HashMap * createMap(long capacity) {
-
-    return NULL;
+HashMap * createMap(long capacity) 
+{
+  
+  
+  
+  return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
