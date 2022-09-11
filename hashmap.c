1adc1e489f915f08df77ecab031f6f1bc1927339
Replit user <>
2022-09-11 20:35:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -137,6 +137,7 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) {
   size_t i = 0;
+  
   if(map->size == 0)
   {
     return NULL;
@@ -152,10 +153,14 @@ Pair * firstMap(HashMap * map) {
   
   for(i=0; i<map->capacity; i++)
   {
-    if(map->buckets[i] != NULL)
+    if(map->buckets[i]->value != NULL)
     {
-      map->current = i;
-      return (map->buckets[i]);
+      if(map->buckets[i]->key != NULL)
+      {
+        map->current = i;
+        return (map->buckets[i]);
+      }
+      
     }
   }
   
